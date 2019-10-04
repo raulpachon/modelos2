67d8d55bd97b1edac1f2a8af040d80f89bed870d
raulpachon <raulpachon97@gmail.com>
2019-10-03 21:35:44 -0500

    Add files via upload

---
+++ RECURSIVAS_e_ITERATIVAS/README.TXT.txt
@@ -0,0 +1,6 @@
+Este programa contine  en forma recursiva e iterativa  las funciones:
+- Término n de la serie de fibonacci
+- Potencia de enteros
+- División de enteros 
+
+codigo desarrollado por Raúl Pachón Alarcón 20171020167
---
+++ RECURSIVAS_e_ITERATIVAS/recursivasIterativas.py
@@ -0,0 +1,39 @@
+# recursivas____________________________________________
+def fibo(n) :
+    if n<=2:return 1
+    return fibo(n-1)+fibo(n-2)
+
+def pot(n,m) :
+    if m==0: return 1
+    if m==1: return n
+    if return pot (m,m-1)*n
+
+def div(n,m):
+    if m>n : return 0
+    return div((n-m),m)+1
+
+#iterativas__________________________________________________
+def fiboi(n) :
+    a,b = 0, 1
+    for i in range(0,n)
+    a,b = b, a+b
+    return a
+
+def poti(n,m) :
+
+res=1
+if n==0
+    res=1
+else:
+    while n<=1:
+        res=res*m
+        n=n-1
+return res
+
+def divi(n,m):
+    res=0
+    n=n-m
+    while n>=0:
+        res=res+1
+        n=n-m
+return res
